762d1a623baee4b2d8ace55e1814bba04e5061f6
Pohorielova <anna.pogorielova@gmail.com>
2023-01-13 02:07:29 +0100

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
+// import { useState } from 'react';
+import { add, removeId } from '../redux/contactsSlice';
+import { setFilter, getFilter } from 'redux/filterSlice';
 import { Box } from './Box';
 import shortid from 'shortid';
 import Form from './Form/Form';
@@ -7,14 +9,17 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
 export default function App() {
-  const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem('contacts')) ?? []
-  );
-  const [filter, setFilter] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(getFilter);
+  // const [contacts, setContacts] = useState(
+  //   JSON.parse(localStorage.getItem('contacts')) ?? []
+  // );
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
   const addName = data => {
     const contact = {
@@ -29,23 +34,32 @@ export default function App() {
 
     contactName.includes(contact.name)
       ? alert(`${contact.name} is already in contacts.`)
-      : setContacts(prevState => [contact, ...prevState]);
+      : // : setContacts(prevState => [contact, ...prevState]);
+        dispatch(
+          add({
+            data,
+            ...contact,
+          })
+        );
   };
 
   const changeFilter = e => {
-    setFilter(e.currentTarget.value);
+    // setFilter(e.currentTarget.value);
+    dispatch(setFilter(e.currentTarget.value));
   };
 
   const deleteContact = contactId => {
-    setContacts(prevState =>
-      prevState.filter(contact => contact.id !== contactId)
-    );
+    // setContacts(prevState =>
+    //   prevState.filter(contact => contact.id !== contactId)
+    // );
+
+    dispatch(removeId(contactId));
   };
 
   const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
+    // const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+      contact.name.toLowerCase().includes(filter)
     );
   };
 
@@ -59,7 +73,6 @@ export default function App() {
       <Box as="div" display="flex" alignItems="center">
         <Box as="div" display="flex" flexDirection="column" width={320}>
           <Form onSubmitForm={addName} contacts={visibleContacts} />
-          {/* <h2>Contacts</h2> */}
           <Filter value={filter} onChange={changeFilter} />
         </Box>
         <ContactList
@@ -70,82 +83,3 @@ export default function App() {
     </Box>
   );
 }
-
-// class OldApp extends Component {
-//   state = {
-//     contacts: [
-//       // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const contacts = localStorage.getItem('contacts');
-//     const parsedContacts = JSON.parse(contacts);
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.contacts !== prevState.contacts) {
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   addName = data => {
-//     const contact = {
-//       id: shortid.generate(),
-//       name: data.name,
-//       number: data.number,
-//     };
-
-//     const contactName = [];
-
-//     this.state.contacts.forEach(contact => contactName.push(contact.name));
-
-//     contactName.includes(contact.name)
-//       ? alert(`${contact.name} is already in contacts.`)
-//       : this.setState(prevState => ({
-//           contacts: [contact, ...prevState.contacts],
-//         }));
-//   };
-
-//   changeFilter = e => {
-//     this.setState({ filter: e.currentTarget.value });
-//   };
-
-//   deleteContact = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-
-//   render() {
-//     const normalizeFilter = this.state.filter.toLowerCase();
-//     const visibleContacts = this.state.contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizeFilter)
-//     );
-
-//     return (
-//       <Box as="div" p={15}>
-//         <h1>Phonebook</h1>
-//         <Box as="div" display="flex" alignItems="center">
-//           <Box as="div" display="flex" flexDirection="column" width={320}>
-//             <Form onSubmitForm={this.addName} contacts={visibleContacts} />
-//             {/* <h2>Contacts</h2> */}
-//             <Filter value={this.state.filter} onChange={this.changeFilter} />
-//           </Box>
-//           <ContactList
-//             contacts={visibleContacts}
-//             onDeleteContact={this.deleteContact}
-//           />
-//         </Box>
-//       </Box>
-//     );
-//   }
-// }
-// export default App;
---
+++ src/redux/filterSlice.jsx
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const filterInitialState = { filter: '' };
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: filterInitialState,
+  reducers: {
+    setFilter(state, action) {
+      return (state.filter = action.payload);
+    },
+  },
+});
+export const { setFilter } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
+export const getFilter = state => filterInitialState.filter;
